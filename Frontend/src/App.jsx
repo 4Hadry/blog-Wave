ae9b18ff07d64741750087835f5630aaf9949917
4Hadry <chammar234@gmail.com>
2023-12-28 14:15:16 +0500

    Folder Stucher , Routers, Header and Footer Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,47 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import PostDetails from "./pages/PostDetails";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import Authors from "./pages/Authors";
+import CreatePosts from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Logout from "./pages/Logout";
+import AuthorsPost from "./pages/AuthorsPosts";
+import CategoryPost from "./pages/CategoryPosts";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "posts/:id", element: <PostDetails /> },
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+      { path: "profile/:id", element: <UserProfile /> },
+      { path: "authors", element: <Authors /> },
+      { path: "create", element: <CreatePosts /> },
+      { path: "posts/:id/edit", element: <EditPost /> },
+      { path: "logout", element: <Logout /> },
+      { path: "myposts/:id", element: <Dashboard /> },
+      { path: "posts/users/:id", element: <AuthorsPost /> },
+      { path: "posts/categories/:category", element: <CategoryPost /> },
+    ],
+  },
+]);
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
---
+++ Frontend/src/pages/AuthorsPosts.jsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+function AuthorsPosts() {
+  return <div>AuthorsPosts</div>;
+}
+
+export default AuthorsPosts;
